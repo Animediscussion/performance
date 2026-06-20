1b102e1f6e03a9e3b80d6e8e504610afec154a97
Animediscussion <prasan1010jeet@gmail.com>
2026-06-21 05:10:37 +0530

    update

---
+++ src/components/List.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+const List = () => {
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+  const handleInput = (e) => {
+    setInput(e.target.value);
+    const newList = [];
+    for (let i = 0; i < 10000000; i++) {
+      newList.push(e.target.value);
+    }
+    setList(newList);
+  };
+  return (
+    <div>
+      <h1>List</h1>
+      <input type="text" value={input} onChange={(e) => handleInput(e)} />
+      {/* <div>List is loaded or building</div> */}
+    </div>
+  );
+};
+
+export default List;
