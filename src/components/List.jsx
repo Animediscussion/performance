69ed2458daff264eac45a9f774332add2a4ed1ca
Animediscussion <prasan1010jeet@gmail.com>
2026-06-22 00:26:21 +0530

    update

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -16,6 +16,9 @@ const List = () => {
       <h1>List</h1>
       <input type="text" value={input} onChange={(e) => handleInput(e)} />
       {/* <div>List is loaded or building</div> */}
+      {list?.map((item, idx) => {
+        return <div key={idx}> {`${item}: ${idx}`}</div>;
+      })}
     </div>
   );
 };
