9e6b8e6ef371046aa21a5c76f731d2f24b53b157
Animediscussion <prasan1010jeet@gmail.com>
2026-06-24 04:45:09 +0530

    update

--- src/components/MemoExample.jsx
+++ src/components/MemoExample.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 
 const MemoExample = () => {
   const [count, setCount] = useState(0);
 
-  const computedValue = (count) => {
+  const expensiveCalculation = (count) => {
     console.log(count);
     for (let i = 0; i < 100000; i++) {}
     return count * 2 || 0;
   };
+
+  const computedValue = useMemo(() => expensiveCalculation(count), [count]);
   return (
     <div>
       <h1>Computed Value: {computedValue(count)}</h1>
