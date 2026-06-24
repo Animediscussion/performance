678c787c8c5e1049d2a2a3488f5ae3836aebaab3
Animediscussion <prasan1010jeet@gmail.com>
2026-06-24 14:32:57 +0530

    update

--- src/components/MemoExample.jsx
+++ src/components/MemoExample.jsx
@@ -1,6 +1,7 @@
 import React, { useMemo, useState } from "react";
 
 const MemoExample = () => {
+  const [val, setVal] = useState("");
   const [count, setCount] = useState(0);
 
   const expensiveCalculation = (count) => {
@@ -12,12 +13,19 @@ const MemoExample = () => {
   const computedValue = useMemo(() => expensiveCalculation(count), [count]);
   return (
     <div>
-      <h1>Computed Value: {computedValue(count)}</h1>
+      <h1>Computed Value: {computedValue}</h1>
       <input
         value={count}
         onChange={(e) => setCount(e.target.value)}
         placeholder="Type something..."
       />
+      <input
+        type="text"
+        value={val}
+        onChange={(e) => setVal(e.target.value)}
+        placeholder="new input "
+      />
+
       <button onClick={() => setCount((prevState) => parseInt(prevState) + 1)}>
         Increment Count
       </button>
