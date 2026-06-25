1a0fb778ef943d29756602e9165db68752b6d000
Animediscussion <prasan1010jeet@gmail.com>
2026-06-26 03:43:50 +0530

    update

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -8,15 +8,20 @@ const Counter = () => {
   };
   return (
     <div>
-      <ShowCount count={count} />
+      <ShowCount count={count} increment={increment} />
+      <br />
       <input type="text" value={val} onChange={(e) => setVal(e.target.value)} />
-      <button onClick={increment}>Increment</button>
     </div>
   );
 };
 
-const ShowCount = ({ count = 0 }) => {
-  return <h1>Here is Count: {count}</h1>;
-};
+const ShowCount = React.memo(({ count = 0, increment }) => {
+  return (
+    <>
+      <h1>Here is Count: {count}</h1>
+      <button onClick={increment}>Increment</button>
+    </>
+  );
+});
 
 export default Counter;
